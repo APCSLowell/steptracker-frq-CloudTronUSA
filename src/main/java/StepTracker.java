36575c546b98a905e95e6026a08a10a4e8526448
CloudTron <96151994+CloudTronUSA@users.noreply.github.com>
2025-04-26 00:12:12 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,16 @@
 import java.util.ArrayList;
-public class StepTracker
-{
- /* to be implemented here */
+public class StepTracker {
+  private int m,d,a,t;
+  public StepTracker(int m) {
+    this.m=m;
+  }
+  public void addDailySteps(int s) {
+    d++;
+    t+=s;
+    if(s>=m) a++;
+  }
+  public int activeDays() { return a; }
+  public double averageSteps() {
+    return d==0 ? 0.0 : (double)t/d;
+  }
 } 
